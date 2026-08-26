84a43aeef559c3fcacf028f6dfb0d94236d9fde1
Yashh027 <yashpathak2776@gmail.com>
2026-08-26 19:53:30 +0530

    V 0.2 Repetition update

--- password_checker.py
+++ password_checker.py
@@ -56,3 +56,22 @@ if has_special:
     print("Contains Special Character : Yes")
 else:    
     print("Contains Special Character : No")
+
+# Checking for repeated characters
+has_repeated = False
+count = 1  # Start at 1 because a single character is a streak of 1
+
+# Start from the 2nd character (index 1)
+for i in range(1, len(user_pass)):
+    if user_pass[i] == user_pass[i - 1]:
+        count += 1
+        if count == 3:
+            has_repeated = True
+            break  # Exit immediately since we found a triple repeat
+    else:
+        count = 1  # Reset streak back to 1 if the characters don't match
+
+if has_repeated:
+    print("Repeated Characters : Suspicious")
+else:
+    print("Repeated Characters : Not Suspicious")
